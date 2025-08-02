e0e1bacd54f6e5e5134dce69a9666469bdcbc30e
masridbd <33666985+masridbd@users.noreply.github.com>
2025-08-03 04:41:04 +0600

    Update date.js

--- api/date.js
+++ api/date.js
@@ -1,11 +1,17 @@
 export default function handler(req, res) {
   const today = new Date();
-  const formattedDate = today.toLocaleDateString('en-CA', { 
+  const date = today.toLocaleDateString('en-CA', { 
     timeZone: 'Asia/Dhaka', 
     year: 'numeric', 
     month: '2-digit', 
     day: '2-digit' 
   }).split('/').reverse().join('-'); // Outputs YYYY-MM-DD, e.g., 2025-08-03
+  const time = today.toLocaleTimeString('en-US', { 
+    timeZone: 'Asia/Dhaka', 
+    hour: 'numeric', 
+    minute: '2-digit', 
+    hour12: true 
+  }).toLowerCase(); // Outputs e.g., 4:38 am
   res.setHeader('Content-Type', 'text/plain');
-  res.end(formattedDate);
+  res.end(`${date}\n${time}`);
 }
