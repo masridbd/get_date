7480221b0ce33c16e2e6dc327dfd5fa7d33d0909
masridbd <33666985+masridbd@users.noreply.github.com>
2025-08-07 18:39:52 +0600

    Update date.js

--- api/date.js
+++ api/date.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const today = new Date();
   const formattedDate = today.toLocaleDateString('en-CA', { 
-    timeZone: 'Asia/Anadyr', 
+    timeZone: 'UTC', 
     year: 'numeric', 
     month: '2-digit', 
     day: '2-digit' 
